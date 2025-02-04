ed2401d390fd55193f112f4066058631b23d0829
Hope <hopeivy@hotmail.co.uk>
2025-02-04 16:35:25 +0000

    Initial commit

---
+++ my-app/client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import ButtonContainer from './components/ButtonContainer'
+import Header from './components/Header'
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <Header />
+    <ButtonContainer />
+    </>
+  )
+}
+
+export default App
---
+++ my-app/client/src/components/ButtonContainer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HotelButton from "./HotelButton";
+
+export default function ButtonContainer({}) {
+  return (
+    <section>
+      <HotelButton />
+      <p>Description</p>
+    </section>
+  );
+}
